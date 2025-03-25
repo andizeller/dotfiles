40c0788b45f45f15a4dd8b9400d1e618993016fc
Andi Zeller <zeller.andi@googlemail.com>
2025-03-25 19:06:24 +0100

    Refactor plugins and add copilot chat

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -12,59 +12,6 @@ return {
       require "configs.lspconfig"
     end,
   },
-
-  -- Copilot
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = {"InsertEnter", "BufEnter", "BufRead"},
-    config = function()
-      require("copilot").setup({
-        panel = {
-          enabled = true,
-          auto_refresh = true,
-          keymap = {
-            jump_prev = "[[",
-            jump_next = "]]",
-            accept = "<CR>",
-            refresh = "gr",
-            open = "<M-CR>"
-          },
-          layout = {
-            position = "bottom", -- | top | left | right
-            ratio = 0.4
-          },
-        },
-        suggestion = {
-          enabled = true,
-          auto_accept = true,
-          auto_trigger = true,
-          debounce = 75,
-          keymap = {
-            accept = "<A-l>",
-            accept_word = false,
-            accept_line = false,
-            next = "<A-j>",
-            prev = "<A-k>",
-            dismiss = "<C-]>",
-          },
-        },
-        filetypes = {
-          yaml = false,
-          markdown = false,
-          help = false,
-          gitcommit = false,
-          gitrebase = false,
-          hgcommit = false,
-          svn = false,
-          cvs = false,
-          ["."] = false,
-        },
-        copilot_node_command = 'node', -- Node.js version must be > 16.x
-        server_opts_overrides = {},
-      })
-    end,
-  },
   {
     "nvim-treesitter/nvim-treesitter",
       run = ":TSUpdate",
@@ -97,15 +44,5 @@ return {
                 under_cursor = true,
             })
     end,
-  },
-  {
-      "Civitasv/cmake-tools.nvim",
-      ft = {"cpp", "c", "cmake"},
-      opts = {
-          cmake_command = "cmake", -- Path to CMake command
-          cmake_build_directory = "build", -- Build directory
-          cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" },
-          cmake_build_options = {"-j24"},
-      },
   },
 }
