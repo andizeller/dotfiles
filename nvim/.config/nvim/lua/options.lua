a1aaa67b5b7679389a6b169e55c45cf8faaec0f0
Andreas Zeller <andreas.zeller@agcocorp.com>
2025-03-31 08:21:22 +0200

    Code folding

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -4,3 +4,6 @@ require "nvchad.options"
 
 local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline!
+o.foldexpr = 'nvim_treesitter#foldexpr()' -- to enable treesitter folding
+o.foldmethod = 'expr' -- to enable treesitter folding
+o.foldenable = false -- do not fold by default
