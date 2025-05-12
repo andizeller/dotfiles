bc5a0bb0a2c111ac510f750e6db88b8e27082546
Andi Zeller <zeller.andi@googlemail.com>
2025-05-12 20:48:28 +0200

    Move cmp to seperate file

---
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,32 @@
+local cmp = require("cmp")
+
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+    },
+    opts = {
+      snippet = {
+        expand = function(args)
+          vim.fn"vsnip#anonymous"
+        end,
+      },
+      mapping = {
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      },
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+      }),
+    }
+  }
+}
+
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -23,16 +23,6 @@ return {
       end,
   },
   {
-    "hrsh7th/nvim-cmp",
-      config = function()
-        require("configs.cmp")
-      end,
-  },
-  {"hrsh7th/cmp-nvim-lsp"},
-  {"hrsh7th/cmp-buffer"},
-  {"hrsh7th/cmp-path"},
-  {"hrsh7th/cmp-cmdline"},
-  {
     "RRethy/vim-illuminate",
     event = {"BufRead", "BufWinEnter"},
     config = function()
