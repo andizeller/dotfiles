de543d65932d4b80a7ba24cac729b982dbf6bad6
Andreas Zeller <andreas.zeller@agcocorp.com>
2025-05-13 08:39:14 +0200

    Add format on save with custom format script

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -9,3 +9,14 @@ map("i", "jk", "<ESC>")
 
 map("v", ">", ">gv", { noremap = true })
 map("v", "<", "<gv", { noremap = true })
+
+-- Define the script to format the file
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*", -- Adjust the pattern to match specific file types if needed (e.g., "*.py")
+    callback = function()
+        vim.fn.system("~/Development/efg_infrastructure_cea_terminals/formatFiles.sh -f " .. vim.fn.expand("%"))
+        vim.cmd("edit")
+    end,
+})
+
+
